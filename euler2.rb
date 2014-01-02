70d3ad123abbf9fa800f4cd9879db65bbebe32e0
Gorev-V <vasilygorev@yandex.ru>
2014-01-02 23:50:29 +0400

    first commit

--- euler2.rb
+++ euler2.rb
@@ -2,12 +2,10 @@ f1=1
 f2=2
 sum=0
 until f1>=4000000 do
-  if f1%2==0
-    sum=sum+f1
-  end
+  sum=sum+f1 if f1%2==0
   puts "f1=#{f1}, f2=#{f2}"
   puts "sum=#{sum}"
   f1, f2 = f2, f2+f1  
 end
-puts "________________"
+puts '________________'
 puts sum
